05d6738a3726b036581491c4c33b1a44bbbf9652
Aditya rai <135973480+aditya9587@users.noreply.github.com>
2024-12-03 21:11:33 +0530

    route change

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,22 @@ import ProductPage from "./pages/Product/ProductPage";
 import { RestaurantProvider } from "./ContextApi/RestaurantContext.jsx";
 import Profile from "./components/profile/Profile.jsx";
 import Checkout from "./pages/checkout/Checkout.jsx";
-// import Address from "./components/Adresss/Address.jsx";
 import Location from "./pages/location/Location.jsx";
 import Payments from "./pages/Payments/Payments.jsx";
-import EditPayrment from "./components/EditPayment/EditPayrment.jsx";
+
 
 function App() {
   return (
     <RestaurantProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<EditPayrment />} />
+          <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/products" element={<ProductPage />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/addAddress" element={<Location />} />
-          <Route path="/payment" element={<Payments /> } />
+          <Route path="/payment" element={<Payments />} />
         </Routes>
         <ToastContainer />
       </BrowserRouter>
